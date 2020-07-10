c5ff3c87af3a97dcfe76d60b5e414897fc8837b2
Dan Seagren <seagrendaniel562@gmail.com>
2020-07-10 16:48:24 -0700

    refactor for object destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (lat, long, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=1bd8d0045e9e84c2792ee6079c46de9b&query=${lat},${long}&units=f`
 
-  request({url: url, json: true}, (err, res) => {
+  request({url, json: true}, (err, {body}) => {
     if(err) {
       callback('Unable to connect to weather service')
-    } else if (res.body.error) {
+    } else if (body.error) {
       callback('Unable to find location')
     } else {
-      const data = res.body.current
+      const data = body.current
       callback(undefined, `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`)
     }
   })
--- utils/geocode.js
+++ utils/geocode.js
@@ -3,16 +3,16 @@ const request = require('request');
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/` + encodeURIComponent(address) + `.json?access_token=pk.eyJ1Ijoic2VhZ3JlbmRhbmllbCIsImEiOiJja2NncWxuY3cwMnA0MndvZXQzMGRyNmM3In0.g04dpahECGR7SECyEUEvHA&limit=1`
 
-  request({ url: url, json: true }, (err, res) => {
+  request({ url, json: true }, (err, {body}) => {
     if (err) {
       callback('Unable to connect to location services')
-    } else if (res.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback('Unable to find location. Try another search.')
     } else {
       callback(undefined, {
-        latitude: res.body.features[0].center[1],
-        longitude: res.body.features[0].center[0],
-        location: res.body.features[0].place_name
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
       })
     }
   })
