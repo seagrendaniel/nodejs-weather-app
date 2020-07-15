564c44322015fdff8f0bbf4848365f21e55ba98b
Dan Seagren <seagrendaniel562@gmail.com>
2020-07-15 15:08:32 -0700

    add weather input to index.html and JS logic to fetch weather from backend API

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,25 @@
-console.log('Client-side JS file is loaded');
+console.log('Client-side JS file is loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+  messageOne.innerHTML = 'Loading..'
+  messageTwo.innerHTML = ''
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.err) {
+        messageOne.innerHTML = data.err
+        messageOne.innerHTML = ''
+      } else {
+        messageOne.innerHTML = data.location
+        messageTwo.innerHTML = data.forecast
+      }
+    })
+  })
+})
